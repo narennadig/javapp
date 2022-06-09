9b5c7e8de8a47cb89f7a74f5d0be9ab564d0756c
Narendranath  Nadig <35164475+narennadig@users.noreply.github.com>
2022-06-08 22:26:53 -0700

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx= SpringApplication.run(DemoApplication.class, args);
+		Alien a=ctx.getBean(Alien.class);
+		a.show();
+	}
+
+
+	}
+
